c01b4c2bcbc6b588480096f9e77fd3357767edcb
Uchenna Nwaigwe <hermessuffice@gmail.com>
2026-08-18 07:40:55 +0100

    End of creating a search microservice - 3

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,7 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,10 +23,20 @@ public class AuctionsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetAuctions()
+    public async Task<ActionResult<List<AuctionDto>>> GetAuctions(string date)
     {
-        var auctions = await _context.Auctions.Include(a => a.Item).OrderBy(x=> x.Item).ToListAsync();
-     return _mapper.Map<List<AuctionDto>>(auctions);
+        var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+
+        if (!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime())>0);
+        }
+
+    //     var auctions = await _context.Auctions.Include(a => a.Item).OrderBy(x=> x.Item).ToListAsync();
+    //  return _mapper.Map<List<AuctionDto>>(auctions);
+
+    return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
+    
     }
 
     [HttpGet("{id}")]
